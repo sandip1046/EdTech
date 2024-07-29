e188b73acbef93e6a19c35353d798ff6efe7791e
Sandip kumar yadav <sandipyadav7541@gmail.com>
2024-07-29 18:18:06 +0530

    final update

--- src/components/course/navCourse.tsx
+++ src/components/course/navCourse.tsx
@@ -2,7 +2,6 @@
 
 import { Link } from "react-router-dom";
 import Logout from "../auth/logOut";
-import ProtectedRouteFullstack from "../../protectedfullstackroute";
 export const NavCourse = () => {
   return (
     <div className={"border-b border-slate-300 bg-slate-200 flex justify-between px-8 py-5"}>
@@ -12,17 +11,8 @@ export const NavCourse = () => {
 
       <div className={"flex justify-evenly items-center gap-1"}>
         <Link to="/courses" className={"flex justify-center flex-col text-cyan-500 font-bold text-xl mx-2 hover:text-slate-500"}>Courses</Link>   {/* this link will route to sigin page onclick  */}
-        {/* <Link to="/fullstack"  className={"flex justify-center flex-col   font-bold text-2xl m-0.5 tracking-wider hover:text-slate-500	  "}>Full Stack</Link> */}
 
 
-        <ProtectedRouteFullstack>
-          <Link
-            to="/fullstack"
-            className={"flex justify-center flex-col font-bold text-xl m-0.5 tracking-wider hover:text-slate-500 "}
-          >
-            Full Stack
-          </Link>
-        </ProtectedRouteFullstack>
         
         <Logout />
 
