3de0f8e4e9c22e69ebcc01853c6056f241378de7
Sandip kumar yadav <sandipyadav7541@gmail.com>
2024-07-29 18:15:08 +0530

    protecting fullstack from the course page

--- src/components/course/navCourse.tsx
+++ src/components/course/navCourse.tsx
@@ -2,6 +2,7 @@
 
 import { Link } from "react-router-dom";
 import Logout from "../auth/logOut";
+import ProtectedRouteFullstack from "../../protectedfullstackroute";
 export const NavCourse = () => {
   return (
     <div className={"border-b border-slate-300 bg-slate-200 flex justify-between px-8 py-5"}>
@@ -12,14 +13,17 @@ export const NavCourse = () => {
       <div className={"flex justify-evenly items-center gap-1"}>
         <Link to="/courses" className={"flex justify-center flex-col text-cyan-500 font-bold text-xl mx-2 hover:text-slate-500"}>Courses</Link>   {/* this link will route to sigin page onclick  */}
         {/* <Link to="/fullstack"  className={"flex justify-center flex-col   font-bold text-2xl m-0.5 tracking-wider hover:text-slate-500	  "}>Full Stack</Link> */}
-        <a
-          href="https://sandip-ed-tech.vercel.app/fullstack"
-          target="_blank"
-          rel="noopener noreferrer"
-          className={"flex justify-center flex-col font-bold text-xl m-0.5 tracking-wider hover:text-slate-500 "}
-        >
-          Full Stack
-        </a>
+
+
+        <ProtectedRouteFullstack>
+          <Link
+            to="/fullstack"
+            className={"flex justify-center flex-col font-bold text-xl m-0.5 tracking-wider hover:text-slate-500 "}
+          >
+            Full Stack
+          </Link>
+        </ProtectedRouteFullstack>
+        
         <Logout />
 
 
