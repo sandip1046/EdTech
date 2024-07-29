f273f06b0373a0faeaf2e745667e32de25140221
Sandip kumar yadav <sandipyadav7541@gmail.com>
2024-07-29 17:54:25 +0530

    adding fullstack button to the course page

--- src/components/course/navCourse.tsx
+++ src/components/course/navCourse.tsx
@@ -7,9 +7,11 @@ export const NavCourse = () => {
       <div className={"border-b border-slate-300 bg-slate-200 flex justify-between px-8 py-5"}>
         
        <Link to="/"  className={"flex justify-center flex-col   font-bold text-2xl m-0.5 tracking-wider hover:text-slate-500	  "}>EdTech</Link>
+      
        
-       <div className={"flex justify-evenly"}>
+       <div className={"flex justify-evenly items-center gap-1"}>
        <Link to="/courses"  className={"flex justify-center flex-col text-cyan-500 font-bold text-xl mx-2 hover:text-slate-500"}>Courses</Link>   {/* this link will route to sigin page onclick  */}
+       <Link to="/fullstack"  className={"flex justify-center flex-col   font-bold text-2xl m-0.5 tracking-wider hover:text-slate-500	  "}>Full Stack</Link>
          <Logout />
        
        
