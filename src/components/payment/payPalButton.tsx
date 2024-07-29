82c1661684edc685c1590b1ddc086ae1b57684f5
Sandip kumar yadav <sandipyadav7541@gmail.com>
2024-07-29 13:14:27 +0530

    Improving the pay button

--- src/components/payment/payPalButton.tsx
+++ src/components/payment/payPalButton.tsx
@@ -35,7 +35,7 @@ export const PayPalButton =() =>{
 
           if (payerId && paymentId) {
               const res = await axios.get(`https://pay-pal-server.vercel.app/?PayerID=${payerId}&paymentId=${paymentId}`);
-
+           
               if (res && res.data.success) {
                   alert("Payment successful!");
                   navigate('/fullstack');
@@ -55,7 +55,7 @@ export const PayPalButton =() =>{
   }, []); //empty dependency means it will chancge in each mount
       return (
         <>
-          <button onClick={handlePayment} className={"flex justify-center items-center rounded-md border-black bg-black border-1 text-white 		mx-5 p-2 h-10	w-2/12 hover:bg-gray-800 xl:mx-3 xl:w-2/12"}>Pay</button>
+          <button onClick={handlePayment} className={"flex justify-center items-center rounded-md border-black bg-black border-1 text-white 		mx-5 p-2 h-10	w-3/12 hover:bg-gray-800 xl:mx-3 xl:w-2/12"}>Pay</button>
         </>
       );
     }
