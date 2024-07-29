d24d63b3ddf08f0565404808b9af487fe78034e3
Sandip kumar yadav <sandipyadav7541@gmail.com>
2024-07-29 17:42:18 +0530

    update the protectedfullstack route

--- src/protectedfullstackroute.tsx
+++ src/protectedfullstackroute.tsx
@@ -1,22 +1,39 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import axios from 'axios';
 import { ReactNode } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from './firebaseAuth';
+import { Navigate } from 'react-router-dom';
 
 interface ProtectedRouteProps {
     children: ReactNode;
-  } 
-const ProtectedRouteFullstack:  React.FC<ProtectedRouteProps> = ({ children }) => {
+}
+const ProtectedRouteFullstack: React.FC<ProtectedRouteProps> = ({ children }) => {
+    const [user, loading, error] = useAuthState(auth);
 
     const hasCompletedPurchase = async (): Promise<boolean> => {
         const response = await axios.get('/api/paypal/payment-status');
         return response.data.completed;
-      };
+    };
 
-  if (!hasCompletedPurchase) {
-    window.location.href = 'https://sandip-ed-tech.vercel.app/payment';
-  }
+    if (loading) {
+        return <div className={"flex justify-center items-center font-extrabold text-4xl m-10"}>Loading...</div>;
+    }
 
-  return <>{children}</>;
+    if (error) {
+        return <div>Error: {error.message}</div>;
+      }
+
+    if (!user) {
+        // Redirect to login page and save the current path to navigate after login
+        return <Navigate to="/signin" state={{ from: location }} replace />;
+      }
+
+    if (!hasCompletedPurchase) {
+        return <Navigate to="/payment" state={{ from: location }} replace />;
+    }
+
+    return <>{children}</>;
 };
 
 export default ProtectedRouteFullstack;
